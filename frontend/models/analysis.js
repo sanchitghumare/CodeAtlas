4201ebd8486b37e60db1a82970c1ea7955b3d36e
Sanchit Ghumare <259650840+sanchitghumare@users.noreply.github.com>
2026-07-26 13:47:45 +0530

    fix: remove required constraint on Analysis.user for guest analyses

--- frontend/models/analysis.js
+++ frontend/models/analysis.js
@@ -5,7 +5,7 @@ const AnalysisSchema = new mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true,
+      default: null,
     },
 
     repository: {
